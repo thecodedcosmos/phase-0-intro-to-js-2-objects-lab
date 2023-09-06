113dc791bc3d66303a0149b0395843d313704e6d
adriannahdbell@gmail.com <120890577+thecodedcosmos@users.noreply.github.com>
2023-09-06 16:49:21 -0700

    Objects Lab Done

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution in this file!
+const employee = {
+  name: "johnnyAppleseed",
+  streetAddress: {
+    streetName: "Mercury Lane", 
+  },
+};
+
+function updateEmployeeWithKeyAndValue() {
+    const employee_1 = {
+        name : "Sam",
+        streetAddress : "11 Broadway",
+    }
+    const totalEmployees = {...employee , ...employee_1};
+    return totalEmployees;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue() {
+    employee.name = "Sam";
+    employee.streetAddress = "12 Broadway";
+    return employee;
+}
+
+function deleteFromEmployeeByKey () {
+    const employee_2 = {
+        race: "White",
+        town: "Murica",
+        status: "Rich",
+    }
+
+    const {race, ...employee_3} = employee_2;
+    return employee_3;
+}
+
+function destructivelyDeleteFromEmployeeByKey() {
+    delete employee.name;
+    return employee;
+}
